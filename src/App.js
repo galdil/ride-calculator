8cdce6bbb434bd907b594c8e6b6b2a0b9e359082
Gal Dayagi <galdil@gmail.com>
2020-08-13 18:56:45 +0300

    remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>test</h1>
-      </header>
+      <h1>בדיקה</h1>
     </div>
   );
 }
