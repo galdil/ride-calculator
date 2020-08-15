5a08ff92bac1d166494d3edcd217b35c6c7ac8c5
Gal Dayagi <galdil@gmail.com>
2020-08-15 16:26:20 +0300

    send to calculator

---
+++ src/components/RideForm/consts.js
@@ -0,0 +1,2 @@
+export const lambdaUrl =
+  "https://ez83ekf7nc.execute-api.eu-west-1.amazonaws.com/prod/uber-predictor";
--- src/components/RideForm/rideForm.js
+++ src/components/RideForm/rideForm.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 
 import Button from "@material-ui/core/Button";
 import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import MenuItem from "@material-ui/core/MenuItem";
 
+import { lambdaUrl } from "./consts";
 import "./rideForm.css";
 
 const submitButtonStyle = {
@@ -52,10 +53,30 @@ const RideForm = () => {
     return !hasError;
   };
 
-  const handleCalculateClicked = async () => {
-    const valid = await validate();
+  const sanitizePayload = (date) => {
+    let currentDays = new Array(7).fill(0);
+    let currentHours = new Array(12).fill(0);
+    const hour = date.toLocaleString("en-US", { hour: "numeric" });
+    const pm = +(hour.slice(2, 4) === "PM");
 
-    if (valid) console.log(input);
+    currentHours[hour.slice(0, 1)] = 1;
+    currentDays[date.getDay()] = 1;
+
+    return `${input.pickUpLong},${input.pickUpLat},${input.dropLong},${input.dropLat},${
+      input.numOfPassengers
+    },${Math.floor(Math.random() * 5000) + 1},${currentDays},${pm},${currentHours}`;
+  };
+
+  const handleCalculateClicked = () => {
+    const valid = validate();
+    const payload = sanitizePayload(new Date());
+    if (valid) {
+      console.log(payload);
+      fetch(lambdaUrl, {
+        method: "post",
+        body: JSON.stringify({ data: payload }),
+      });
+    }
   };
 
   return (
@@ -64,12 +85,12 @@ const RideForm = () => {
         <FormControl>
           <InputLabel htmlFor="age-native-simple">Number of passengers</InputLabel>
           <Select value={input.numOfPassengers} name="numOfPassengers" onChange={handleChange}>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={"1"}>1</MenuItem>
+            <MenuItem value={"2"}>2</MenuItem>
+            <MenuItem value={"3"}>3</MenuItem>
+            <MenuItem value={"4"}>4</MenuItem>
+            <MenuItem value={"5"}>5</MenuItem>
+            <MenuItem value={"6"}>6</MenuItem>
           </Select>
         </FormControl>
 
